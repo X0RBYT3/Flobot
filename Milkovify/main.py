69d88bddcf784d69b74c635409438a62fae95fae
Nekurone <zx627924@ou.ac.uk>
2021-08-16 17:15:55 +0100

    Update to include new logging

--- Milkovify/main.py
+++ Milkovify/main.py
@@ -1,17 +1,22 @@
+import logging
+
 from bot import Client
-from Core.config import logger, TOKEN
+from Core.config import TOKEN
+
+log = logging.getLogger(__name__) # Logging not actually used here, mostly. 
 
 """
 Runs the client. When adding new Cogs. Be sure to add them to COGS in here so the bot can load them.
 
 todo: Rewrite
 """
+
 COGS = ["owner", "stats", "utils", "music"]
 
 
 def main():
     client = Client(COGS)
-    logger.GENERAL("Booting FloBot with {0} Cogs.".format(len(COGS)))
+    print("Booting FloBot with {0} Cogs.".format(len(COGS)))
     client.run(TOKEN)
 
 
